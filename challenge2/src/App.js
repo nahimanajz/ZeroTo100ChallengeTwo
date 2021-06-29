ff86cbf279494a80abb6c25f8944a9a87fab32f6
nahimanajz <jazzonahimana@gmail.com>
2021-06-29 18:12:31 +0200

    refactor: Add reload

--- challenge2/src/App.js
+++ challenge2/src/App.js
@@ -2,51 +2,55 @@ import {useEffect,useState} from 'react'
 import axios from 'axios'
 import {URL_INDEX} from './config'
 import { Photo } from './components/Photo'
+import Loader from 'react-loader'
 
 function App() {
   const [albumId, setAlbumId] = useState('')
-  const[isLoading, setIsLoading] = useState(false)
   const [photos,setPhotos] = useState(null) //{} of title and Image thumbnail
   const[reload, setReload] = useState(false)
-  
-  const fetchPhotos = async () => {
-    
-   
-}
-  useEffect( ()=>{
+ const queryChange =(e) =>{
+  setPhotos(null)
+  setAlbumId(e.target.value)
+ }
+  useEffect(()=>{
 
-      setIsLoading(true)
       if(albumId && reload){
         ( async()=>{
-          return await axios.get(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`)
-          .then(({data})=>{
+          const { data } = await axios.get(`${URL_INDEX}/albums/${albumId}/photos`)
             setPhotos(data)
             setReload(false)
-          })
-          .catch(err => console.log(err))
+                      
         })()
       }
 
-  },[photos,isLoading,albumId,reload])
+  },[photos,albumId,reload])
 
   return (<>
-          <header></header>
-          <main>
-            
-              <input type="number" onChange={(e)=>setAlbumId(e.target.value)}/>
-              <button onClick={(e)=>setReload(true)}> Get Album Photos By Id</button>
-            
-
-            { photos &&
-              photos.map(({thumbnailUrl, title}) =>(
-                <Photo
-                  key={title} 
-                  imgSrc={thumbnailUrl}
-                  title={title} />
-              ))
-              
-            } 
-          </main>
+          <div className="header">
+            <div className="logo">ZeroTo100 </div>           
+          </div>
+          <div className="main">            
+            <div className="panel">
+              <div className="form">
+                  <h1>Albums and Photo</h1>
+                  <input type="number" onChange={queryChange} placeholder='Enter Album ID'/>
+                  <button onClick={()=>setReload(true)}> Get Album Photos By Id</button>
+                </div>
+            </div>
+            <div className="album-photo">
+                {reload && !photos ? <Loader className='spinner' speed={0.5}/> :(                
+                      photos && photos.map(({thumbnailUrl, title}) =>(                        
+                          <Photo
+                            key={title} 
+                            imgSrc={thumbnailUrl}
+                            title={title} />                          
+                      ))
+                )
+              }
+            </div>
+          
+          </div>
+         
        </>
   );
 }
